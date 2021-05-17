41d1e822eea856c2c074a1656a7b4e060f217d4c
Stephen Nguyen <snguy3nn@gmail.com>
2021-05-17 12:00:31 -0500

    created objects to fill herd

--- herd.py
+++ herd.py
@@ -6,4 +6,10 @@ class Herd:
         self.dinosaurs = []
         self.create_herd()
 
-
+    def create_herd(self):
+        dino1 = Dinosaur('T-Rex', 100)
+        dino2 = Dinosaur('Raptor', 100)
+        dino3 = Dinosaur('Triceratops', 100)
+        self.dinosaurs.append(dino1)
+        self.dinosaurs.append(dino2)
+        self.dinosaurs.append(dino3)
