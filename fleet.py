23bbe403c6a603a7c4f2356ad8afa0cba50ec991
Stephen Nguyen <snguy3nn@gmail.com>
2021-05-17 12:00:55 -0500

    created objects to fill fleet

--- fleet.py
+++ fleet.py
@@ -6,3 +6,10 @@ class Fleet:
         self.robots = []
         self.create_fleet()
 
+    def create_fleet(self):
+        robot1 = Robot("Bumblebee")
+        robot2 = Robot("Optimus Prime")
+        robot3 = Robot("Grimlock")
+        self.robots.append(robot1)
+        self.robots.append(robot2)
+        self.robots.append(robot3)
